29917471844c4451816ea6abe33af51e36e765f6
rezamsani <rezamsanitrrga@gmail.com>
2025-03-26 02:30:35 +0330

    set-route-on-user-service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,18 +1,24 @@
 import { Subject } from "rxjs";
 import { AuthData } from "./auth.model";
 import { User } from "./user.model";
+import { Router } from '@angular/router';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class AuthService {
     authChange = new Subject<boolean>();
     private user: User | null = null;
 
-  
+
+  constructor(private router:Router) {    
+    
+  }
     registerUser(authData: AuthData) {
       this.user = {
         email: authData.email,
         userId: Math.round(Math.random() * 10000).toString(),
       };
-      this.authChange.next(true);
+      this.successFulAuth();
     }
   
     login(authData: AuthData) {
@@ -20,12 +26,13 @@ export class AuthService {
         email: authData.email,
         userId: Math.round(Math.random() * 10000).toString(),
       };      
-      this.authChange.next(true);      
+      this.successFulAuth();
     }
   
     logout() {
       this.user = null;
       this.authChange.next(false);
+      this.router.navigate(['/login']);    
     }
   
     getUser() {
@@ -34,6 +41,10 @@ export class AuthService {
   
     isAuth() {
       return this.user != null;
+    }
+    successFulAuth(){
+      this.authChange.next(true); 
+      this.router.navigate(['/training']);
     }
   }
   
--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -37,5 +37,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   onToggleSidenav() {
     this.sidenavToggle.emit();
   }
-
+  onLogout(){
+    this.authService.logout();
+  }
 }
