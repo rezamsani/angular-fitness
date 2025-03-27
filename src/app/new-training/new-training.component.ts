e8bff065e982796b4e5871a76f144fc02e2af23a
rezamsani <rezamsanitrrga@gmail.com>
2025-03-27 23:45:59 +0330

    using-snapshotchange-method

--- src/app/new-training/new-training.component.ts
+++ src/app/new-training/new-training.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Firestore, collection, collectionData, collectionSnapshots } from '@angular/fire/firestore';
+import { Observable, map } from 'rxjs';
 import { FormsModule, NgForm } from '@angular/forms';
 import { FlexLayoutModule } from 'ngx-flexible-layout';
 import { CommonModule } from '@angular/common';
@@ -18,7 +18,7 @@ import { Exercise } from '../training/exercise.model';
 export class NewTrainingComponent implements OnInit {
   private firestore = inject(Firestore); // ✅ نسخه جدید با Standalone API
 
-  exercises: Observable<any[]>; // ✅ مقدار قابل مشاهده برای استفاده در کامپوننت
+  exercises: Observable<Exercise[]>; // ✅ مقدار قابل مشاهده برای استفاده در کامپوننت
   constructor(private trainintService: TrainingService) {
 
 
@@ -27,14 +27,24 @@ export class NewTrainingComponent implements OnInit {
     //this.exercises = this.trainintService.getAvailableExercises();
 
     const exercisesCollection = collection(this.firestore, 'exercises');
-    this.exercises = collectionData(exercisesCollection);
+    this.exercises = collectionSnapshots(exercisesCollection).pipe(
+      map(docArray =>
+        docArray.map(doc => ({
+          id: doc.id, // `doc.id` مستقیم مقدار دارد
+          name: doc.data()['name'], // مقدار را مستقیم می‌خوانیم
+          duration: doc.data()['duration'],
+          calories: doc.data()['calories']
+        }))
+      )
+    );
+
 
     // this.exercises$.subscribe(data => {
     //   this.exercises$ = data;
     // });
 
   }
-  onStartTraining(form: NgForm) {    
+  onStartTraining(form: NgForm) {
     this.trainintService.startExercise(form.value.excercise);
   }
 }
