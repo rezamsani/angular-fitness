644560800babcda67bbbe1790d04f10c73d798f9
rezamsani <rezamsanitrrga@gmail.com>
2025-03-28 14:20:43 +0330

    done-authenttication-with-firbase

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,50 +1,51 @@
 import { Subject } from "rxjs";
 import { AuthData } from "./auth.model";
-import { User } from "./user.model";
+import { UserModel } from "./user.model";
 import { Router } from '@angular/router';
 import { Injectable } from "@angular/core";
-
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, User } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 @Injectable()
 export class AuthService {
-    authChange = new Subject<boolean>();
-    private user: User | null = null;
+  authChange = new Subject<boolean>();
+  private user: User | null = null;
 
-
-  constructor(private router:Router) {    
-    
-  }
-    registerUser(authData: AuthData) {
-      this.user = {
-        email: authData.email,
-        userId: Math.round(Math.random() * 10000).toString(),
-      };
+  constructor(private router: Router, private auth: Auth) { }
+  async registerUser(authData: AuthData) {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        this.auth, authData.email, authData.password);
+      this.user = userCredential.user;
+      console.log('User registered successfully!', userCredential.user);
       this.successFulAuth();
-    }
-  
-    login(authData: AuthData) {
-      this.user = {
-        email: authData.email,
-        userId: Math.round(Math.random() * 10000).toString(),
-      };      
-      this.successFulAuth();
-    }
-  
-    logout() {
-      this.user = null;
-      this.authChange.next(false);
-      this.router.navigate(['/login']);    
-    }
-  
-    getUser() {
-      return { ...this.user };
-    }
-  
-    isAuth() {
-      return this.user != null;
-    }
-    successFulAuth(){
-      this.authChange.next(true); 
-      this.router.navigate(['/training']);
+    } catch (error) {
+      console.error('Registration error:', error);
     }
   }
-  
+  login(authData: AuthData) {
+    // this.user = {
+    //   email: authData.email,
+    //   userId: Math.round(Math.random() * 10000).toString(),
+    // };
+    this.successFulAuth();
+  }
+
+  logout() {
+    this.user = null;
+    this.authChange.next(false);
+    this.router.navigate(['/login']);
+  }
+
+  getUser() {
+    return { ...this.user };
+  }
+
+  isAuth() {
+    return this.user != null;
+  }
+  successFulAuth() {
+    this.authChange.next(true);
+    this.router.navigate(['/training']);
+  }
+}
--- src/app/auth/user.model.ts
+++ src/app/auth/user.model.ts
@@ -1,4 +1,4 @@
-export interface User {
+export interface UserModel {
     email: String;
     userId: String;
   }
